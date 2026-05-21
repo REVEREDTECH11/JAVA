58245571a20723377b597555a4d41149b940fb71
GREG LAMAR <Greglamar2001@gmail.com>
2026-05-21 09:06:27 -0400

    JAVA DAY 1
    JAVA DAY 1 UPLOAD

---
+++ JAVA SUMMER 2026 REFRESH/JAVADAY1/src/Hello.java
@@ -0,0 +1,52 @@
+//SUMMER JAVA REFRESHER DAY ONE
+/*
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        //ln adds newline
+        System.out.println("How are you doing today?");
+
+        System.out.println("Hello World!\nHow are you doing today?");
+
+    }
+
+}
+*/
+
+//Exercise 1.1
+/*1. In computer jargon, what’s the difference between a statement and a
+comment?*/
+//A statement specifies one step of an algorithm
+//A comment has no effect but contains information
+
+//2. What does it mean to say that a program is portable?
+//You can take that exact code and run it on another program
+
+//3. In common English, what does the word compile mean?
+//Compile means to translate a high level language program into a low level language.
+//4. What is an executable? Why is that word used as a noun?
+//A runnable task for a computer. Because it is object code.
+
+//Exercise 1.2
+/*
+1. Type in the Hello World program; then compile and run it.
+2. Add a print statement that displays a second message after the Hello,
+World!. Say something witty like, How are you?. Compile and run the
+program again*/
+
+public class Hello {
+
+    public static void main(String[] args) {
+        //PART 1
+        System.out.println("Helo World!!!");
+        //PART 2
+        System.out.println("How are you doing today");
+        System.out.println("");
+        //PART 1 & 2
+        System.out.println("Helo World!!!\nHow are you doing today");
+    }
+
+}
+
